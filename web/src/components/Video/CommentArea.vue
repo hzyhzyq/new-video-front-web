b3d12d6308557f3ef205040b2b26bd156fcd0777
hzyhzyq <z200024@163.com>
2022-04-21 18:01:49 +0800

    4.21

--- web/src/components/Video/CommentArea.vue
+++ web/src/components/Video/CommentArea.vue
@@ -1,15 +1,20 @@
 <template>
-  <div style="position: relative; width: 100%">
-    <el-row>
+  <div style="position: relative; width: 100%;height: auto">
+    <el-row style="height: auto">
       <el-col :span="4">
         <div class="grid-content bg-purple">
           <div class="avatar">
             <el-avatar :size="50" :src="circleUrl"></el-avatar>
           </div>
+          <p>UserName</p>
         </div>
       </el-col>
-      <el-col :span="20">
-        <div class="grid-content bg-purple-light"></div>
+      <el-col :span="20" style="height: auto">
+        <div class="grid-content bg-purple-light">
+          <div style="position: relative; width: 90%; height:auto;left: 5%;text-align: left;top:45px">
+            <p>This is a  dsaddsdaddaaaaa dsaddsdad dsaddsdaddaaaaadsaddsdaddaaaaadsaddsdaddaaaaadsaddsdaddaaaaadsaddsdaddaaaaadaaaaadsaddsdaddaaaaa  dsaddsdaddaaaaadsaddsdaddaaaaadsaddsdaddaaaaadsaddsdaddaaaaadsaddsdaddaaaaadsaddsdaddaaaaa dsaddsdaddaaaaadsaddsdaddaaaaadsaddsdaddaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+          </div>
+        </div>
       </el-col>
     </el-row>
   </div>
@@ -28,6 +33,15 @@ export default {
 </script>
 
 <style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  font-size: 10px;
+}
+p{
+  font-size: 16px;
+  word-wrap:break-word;
+}
 .bg-purple {
   background: #d3dce6;
 }
@@ -36,14 +50,18 @@ export default {
   background: #e5e9f2;
 }
 .grid-content {
+  position: relative;
   border-radius: 4px;
-  min-height: 200px;
+  height: auto;
+  text-align: center;
 }.avatar{
    position: relative;
    width: 50px;
    height: auto;
    left: 0;
    right: 0;
-   margin: auto;
+   top:20px;
+   margin: auto auto 30px;
+   text-align: center;
  }
 </style>
